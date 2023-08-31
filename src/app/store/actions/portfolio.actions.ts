ea880d3033e4fcacd738fe53044de679ba179f8d
Thomas <thomas.ben@nestgroup.net>
2023-08-31 13:33:24 +0530

    Auto-generated commit [ci skip]

--- src/app/store/actions/portfolio.actions.ts
+++
@@ -1,6 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-import { Project } from '../../models/project.model';
-
-export const loadProjects = createAction('[Portfolio] Load Projects');
-export const loadProjectsSuccess = createAction('[Portfolio] Load Projects Success', props<{ projects: Project[] }>());
-export const loadProjectsFailure = createAction('[Portfolio] Load Projects Failure', props<{ error: any }>());
--- src/app/store/selectors/portfolio.selectors.ts
+++
@@ -1,9 +0,0 @@
-import { createSelector, createFeatureSelector } from '@ngrx/store';
-import { PortfolioState } from '../reducers/portfolio.reducer';
-
-export const selectPortfolioState = createFeatureSelector<PortfolioState>('portfolio');
-
-export const selectProjects = createSelector(
-  selectPortfolioState,
-  state => state.projects
-);
